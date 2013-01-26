6d655a8276ca8298cf09e7ce9610abfd08040d57
Bryan Marty <bxm156@case.edu>
2013-01-26 15:16:39 -0800

    Updated SQL

---
+++ Smartility/apps/categories/sql/category.mysql.sql
@@ -0,0 +1,17 @@
+INSERT INTO `categories_category` (`id`, `name`)
+VALUES
+	(1, 'Entertainment'),
+	(2, 'TVs'),
+	(3, 'HD DVR'),
+	(4, 'Gaming'),
+	(5, 'Audio'),
+	(6, 'Office'),
+	(7, 'Desktops'),
+	(8, 'Monitors'),
+	(9, 'Laptops'),
+	(10, 'Chargers'),
+	(11, 'Appliances'),
+	(12, 'Kitchen'),
+	(13, 'Heating'),
+	(14, 'Cooling'),
+	(15, 'Lighting');
