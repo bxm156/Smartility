f19d955d7b8f908621eb71522323be83479e766f
Bryan Marty <bxm156@case.edu>
2013-01-27 14:23:52 -0800

    Achieved basic graph!

--- Smartility/apps/energydata/views.py
+++ Smartility/apps/energydata/views.py
@@ -1,13 +1,12 @@
 # Create your views here.
-import json
+import simplejson as json
 from random import choice
-from random import randrange
 from itertools import groupby
+from django.core.serializers.json import DjangoJSONEncoder
 
 from django.http import HttpResponse
 
 from models import EnergyDataPoint
-    
 
 def get_data(request, user_id):
     response_data = {'message':'failed'}
@@ -15,7 +14,7 @@ def get_data(request, user_id):
     start_time = request.GET.get('start','2012-01-01 00:00:00+00:00')
     end_time = request.GET.get('end', '2012-02-01 00:00:00+00:00')
     category = request.GET.get('cat', None)
-    granularity = request.GET.get('gran',None)
+    granularity = request.GET.get('gran', "day")
     
     datapoints = EnergyDataPoint.objects.filter(start_time__gte=start_time,end_time__lte=end_time)
     if category:
@@ -24,10 +23,11 @@ def get_data(request, user_id):
 
     if granularity == "day":
         for start_date, group in groupby(datapoints, key=lambda x: x.start_time.date()):
-            response_data['data'].append([start_date.__str__(), sum(data.value for data in list(group)).__str__()])
+            response_data['data'].append(sum(data.value for data in list(group)))
+            response_data['title'] = "Month of %s" % (start_date.strftime("%B"),)
     else:
         for point in datapoints:
-            response_data['data'].append([point.start_time.__str__(), point.end_time.__str__(), point.category_id.__str__(), point.value.__str__()])
+            response_data['data'].append([point.start_time.__str__(), point.end_time.__str__(), point.category_id.__str__(), point.value])
     response_data['message'] = 'success'
     response_data['user'] = user_id
     return HttpResponse(json.dumps(response_data), content_type="application/json")
